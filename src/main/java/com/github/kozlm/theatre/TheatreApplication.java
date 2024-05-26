f471d7d6d1b204ca929818668c92f803905a2806
Michał Kozłowski <m.kozlowski181103@gmail.com>
2024-05-27 00:02:53 +0200

    Initial commit

---
+++ src/main/java/com/github/kozlm/theatre/TheatreApplication.java
@@ -0,0 +1,13 @@
+package com.github.kozlm.theatre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheatreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheatreApplication.class, args);
+	}
+
+}
