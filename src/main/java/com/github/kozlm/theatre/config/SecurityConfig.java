a89fb19efa9be99029d1ccddbce92ca176f24cc1
Michał Kozłowski <m.kozlowski181103@gmail.com>
2024-09-03 19:20:33 +0200

    Add Jwt filter

--- src/main/java/com/github/kozlm/theatre/config/SecurityConfig.java
+++ src/main/java/com/github/kozlm/theatre/config/SecurityConfig.java
@@ -1,17 +1,23 @@
 package com.github.kozlm.theatre.config;
 
+import com.github.kozlm.theatre.security.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    AuthenticationProvider authenticationProvider;
+    JwtAuthenticationFilter jwtFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
@@ -21,7 +27,11 @@ public class SecurityConfig {
                         .requestMatchers("/management/**").hasRole("ADMIN")
 //                        .requestMatchers("/").hasRole("CLIENT")
                         .anyRequest().permitAll()
-                );
+                )
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
